905c9fc06781973e90dc314e10db83be98d4ee48
Teddy <vrosa.ted@technomar.com.br>
2023-08-15 02:17:55 -0300

    feat(server): servidor criado com rota base de teste
    Arquivo de servidor e de rotas criado, com uma rota retornando Hello World para testar se o servidor
    esta de pé

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Router } from 'express';
+import morgan from 'morgan';
+import { router } from './routes';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use(router)
+
+export { app }
