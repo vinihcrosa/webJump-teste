7d468f11bf46e3d03eab0d3e48ecb01f0f36a69b
Teddy <vrosa.ted@technomar.com.br>
2023-08-16 17:49:14 -0300

    feat(getallcategories): creat route to get all categories

--- src/useCases/categoryUseCases/routes.ts
+++ src/useCases/categoryUseCases/routes.ts
@@ -1,10 +1,15 @@
 import { Request, Response, Router } from "express";
 import { createCategoryController } from "./createCategory";
+import { getAllCategoriesController } from "./getAllCategories";
 
 const categoryRouter = Router()
 
 categoryRouter.post('/', (req: Request, res: Response) => {
   createCategoryController.handle(req, res)
+})
+
+categoryRouter.get('/', (req: Request, res: Response) => {
+  getAllCategoriesController.handle(req, res)
 })
 
 export { categoryRouter }
