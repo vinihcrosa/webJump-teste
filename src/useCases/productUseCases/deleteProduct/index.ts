d50f486b949cda4d4b2f8c0def3d75874dc522d5
Teddy <vrosa.ted@technomar.com.br>
2023-08-17 01:24:36 -0300

    chore(deleteproduct): add logs to deleteProduct route

--- src/useCases/productUseCases/deleteProduct/index.ts
+++ src/useCases/productUseCases/deleteProduct/index.ts
@@ -1,8 +1,9 @@
+import { pinoLogger } from "../../../Logger/pino";
 import { productRepository } from "../../../repositories/implementations/prisma";
 import { DeleteProductController } from "./deleteProduct.controller";
 import { DeletyeProductUseCase } from "./deleteProduct.useCase";
 
-const deleteProductUseCase = new DeletyeProductUseCase(productRepository);
-const deleteProductController = new DeleteProductController(deleteProductUseCase);
+const deleteProductUseCase = new DeletyeProductUseCase(productRepository, pinoLogger);
+const deleteProductController = new DeleteProductController(deleteProductUseCase, pinoLogger);
 
 export { deleteProductController, deleteProductUseCase };
