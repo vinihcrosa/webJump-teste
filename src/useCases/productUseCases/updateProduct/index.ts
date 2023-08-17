80f576bc5e1b4e15f1b03c30ba86c2faf845f85c
Teddy <vrosa.ted@technomar.com.br>
2023-08-17 01:43:09 -0300

    chore(updateproduct): add logs to update prot route

--- src/useCases/productUseCases/updateProduct/index.ts
+++ src/useCases/productUseCases/updateProduct/index.ts
@@ -1,9 +1,10 @@
+import { pinoLogger } from "../../../Logger/pino";
 import { productRepository } from "../../../repositories/implementations/prisma";
 import { UpdateProductController } from "./updateProduct.controller";
 import { UpdateProductUseCase } from "./updateProduct.useCase";
 
 
-const updateProductUseCase = new UpdateProductUseCase(productRepository);
-const updateProductController = new UpdateProductController(updateProductUseCase);
+const updateProductUseCase = new UpdateProductUseCase(productRepository, pinoLogger);
+const updateProductController = new UpdateProductController(updateProductUseCase, pinoLogger);
 
 export { updateProductUseCase, updateProductController };
